93bb82be42f236b8d9587f212feef21855fdd042
Michael Kucinski <michaelkucinski@gmail.com>
2025-12-31 11:39:34 -0500

    feat: syntax highlighting

--- src/ui.rs
+++ src/ui.rs
@@ -1,16 +1,16 @@
 use crate::app::{Action, FileExplorerApp};
 use egui::Color32;
 
-use iced::widget::scrollable;
+use iced::widget::text::{Rich, Span};
+use iced::widget::{scrollable};
 use iced::{
     Background, Color, Font, Length,
     font::Weight,
-    widget::{
-        button, column, container, row,
-        scrollable::{Direction, Scrollbar},
-        space, text,
-    },
+    widget::{button, column, container, row, space, span, text},
 };
+
+use syntect::easy::HighlightLines;
+use syntect::util::LinesWithEndings;
 use syntect::{highlighting::ThemeSet, parsing::SyntaxSet};
 
 /// Implement the eframe App trait (interface) for the FileExplorerApp
@@ -219,7 +219,8 @@ impl eframe::App for FileExplorerApp {
     }
 }
 
-const HEADING_FONT_SIZE: f32 = 24.0;
+const HEADING_FONT_SIZE: f32 = 32.0;
+const FILE_NAME_FONT_SIZE: f32 = 24.0;
 
 impl FileExplorerApp {
     pub fn update(&mut self, action: Action) {
@@ -254,17 +255,22 @@ impl FileExplorerApp {
             }
         };
 
-        let back_button: iced::Element<Action> =
-            button(row![text("📂 ../").shaping(text::Shaping::Advanced)])
-                .on_press(Action::GoBack())
-                .style(selected_file_theme(false))
-                .width(Length::Fill)
-                .into();
+        let back_button: iced::Element<Action> = button(row![
+            text("⬆️ ../")
+                .shaping(text::Shaping::Advanced)
+                .size(FILE_NAME_FONT_SIZE)
+        ])
+        .on_press(Action::GoBack())
+        .style(selected_file_theme(false))
+        .width(Length::Fill)
+        .into();
 
         let mut file_nodes: Vec<iced::Element<Action>> = Vec::new();
 
         for (index, f) in self.files.iter().enumerate() {
-            let file_name_row = text(f.display_name()).shaping(text::Shaping::Advanced);
+            let file_name_row = text(f.display_name())
+                .shaping(text::Shaping::Advanced)
+                .size(FILE_NAME_FONT_SIZE);
 
             let is_selected = match &self.opened_file {
                 Some(opened_file) => opened_file.absolute_path == f.absolute_path,
@@ -280,24 +286,67 @@ impl FileExplorerApp {
             );
         }
 
-        let content = match &self.opened_file {
-            Some(opened_file) => match &self.opened_file_lines {
-                Ok(lines) => {
-                    let mut text_lines: Vec<iced::Element<Action>> = Vec::new();
-                    for (index, line) in lines.iter().enumerate() {
-                        text_lines.push(
-                            row![
-                                text(format!("{:4}", index + 1)).font(Font::MONOSPACE),
-                                space::vertical().width(Length::Fixed(10.0)),
-                                text(line)
-                                    .shaping(text::Shaping::Advanced)
-                                    .font(Font::MONOSPACE)
-                            ]
-                            .into(),
-                        );
-                    }
+        let content = container(self.file_contents());
 
-                    // The main content column
+        row![
+            container(
+                column![
+                    text(self.opened_dir.display_name())
+                        .size(HEADING_FONT_SIZE)
+                        .font(Font {
+                            weight: Weight::Bold,
+                            ..Font::default()
+                        }),
+                    scrollable(column![
+                        back_button,
+                        iced::widget::Column::from_vec(file_nodes).width(Length::Fill)
+                    ])
+                ]
+                .width(Length::Fill),
+            )
+            .width(Length::FillPortion(1)),
+            content.width(Length::FillPortion(4)),
+        ]
+        .spacing(20.0)
+        .into()
+    }
+
+    fn file_contents(&self) -> iced::Element<'_, Action> {
+        let result = match &self.opened_file {
+            Some(opened_file) => match &self.opened_file_contents {
+                Ok(contents) => {
+                    let ps = SyntaxSet::load_defaults_newlines();
+                    let ts = ThemeSet::load_defaults();
+                    let syntax = ps
+                        .find_syntax_by_extension(&self.opened_file_type.clone().unwrap_or(String::from("txt")))
+                        .or(ps.find_syntax_by_extension("txt"))
+                        .unwrap();
+                    let theme = &ts.themes["base16-ocean.dark"]; // A popular dark theme
+                    let mut h = HighlightLines::new(syntax, theme);
+
+                    let highlighted = iced::widget::Column::with_children(
+                        LinesWithEndings::from(&contents)
+                            .map(|line| {
+                                h.highlight_line(line, &ps)
+                                    .unwrap()
+                                    .iter()
+                                    .map(|(style, text)| {
+                                        span(*text)
+                                            .color(Color::from_rgb(
+                                                style.foreground.r as f32 / 255.0,
+                                                style.foreground.g as f32 / 255.0,
+                                                style.foreground.b as f32 / 255.0,
+                                            ))
+                                            .font(Font::MONOSPACE)
+                                            .into()
+                                    })
+                                    .collect::<Vec<Span<String, Font>>>()
+                            })
+                            .map(|spans| Rich::with_spans(spans))
+                            .map(iced::Element::from)
+                            .collect::<Vec<_>>(),
+                    );
+
                     column![
                         row![
                             text(&opened_file.file_name)
@@ -314,14 +363,19 @@ impl FileExplorerApp {
                             )
                             .padding(10.0)
                         ],
-                        scrollable(iced::widget::Column::from_vec(text_lines))
+                        scrollable(highlighted)
                             .width(Length::Fill)
                             .height(Length::Fill)
                     ]
                     .spacing(20.0)
                 }
                 Err(e) => {
-                    column![text(format!("Error: {}", e))]
+                    column![
+                        text(format!("Error: {}", e))
+                        .size(FILE_NAME_FONT_SIZE)
+                        .font(Font::MONOSPACE)
+                        .color(Color::from_rgb(1.0, 0.0, 0.0))
+                    ]
                 }
             },
             None => {
@@ -334,32 +388,7 @@ impl FileExplorerApp {
             }
         };
 
-        row![
-            container(
-                column![
-                    scrollable(row![
-                        text(self.opened_dir.display_name())
-                            .size(HEADING_FONT_SIZE)
-                            .font(Font {
-                                weight: Weight::Bold,
-                                ..Font::default()
-                            })
-                            .wrapping(text::Wrapping::None),
-                    ])
-                    .direction(Direction::Horizontal(
-                        Scrollbar::new().width(5).margin(0).scroller_width(5)
-                    )),
-                    scrollable(column![
-                        back_button,
-                        iced::widget::Column::from_vec(file_nodes).width(Length::Fill)
-                    ])
-                ]
-                .width(Length::Fill),
-            )
-            .width(Length::FillPortion(1)),
-            content.width(Length::FillPortion(4)),
-        ]
-        .spacing(20.0)
-        .into()
+        column!(result).into()
     }
+
 }
